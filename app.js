cdb27297d2b440c944d87a96a1ec2d9d36a708b0
tsuy <spaceminer@protonmail.com>
2021-01-12 14:43:15 -0500

    only use dotenv in development

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const dotenv = require('dotenv').config()
+
+if (process.env.NODE_ENV == 'development') {
+    const dotenv = require('dotenv').config()
+}
 
 const routes = require('./routes')
 
 require('./config/mongoose')
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 

09ffa494ea881ad77c7bd4d874c02a846eb79190
spacemines <t.nych.w@gmail.com>
2021-01-13 21:25:47 -0500

    testing

--- app.js
+++ app.js
@@ -12,7 +12,15 @@ const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(cors())
+router.get('/', function(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+    res.send('cors problem fixed:)');
+})
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 

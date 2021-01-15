a769d7733dd21c286a1c7dbd9a1ffda4091a145d
spacemines <t.nych.w@gmail.com>
2021-01-15 00:28:04 -0500

    .

--- routes/users.js
+++ routes/users.js
@@ -132,9 +132,9 @@ router.post('/cart-items', async (req, res) => {
             message.message = 'item not found'
             return res.status(400).json(message)
         }
-
+        console.log(item)
         // add it to user's cart
-        user.cart.set(itemname, { item.price, item.description, quantity })
+        user.cart.set(itemname, {...item}) 
         user.save()
         message.message = 'item added'
         message.user = user
